4b00c8d70f10b1d0d6b9548ce106669ab478be42
GyanP <gyanig72@gmail.com>
2021-06-28 16:50:21 +0530

    setup component library

--- job-search/src/components/JobSearch/index.jsx
+++ job-search/src/components/JobSearch/index.jsx
@@ -1,5 +1,8 @@
+import React from 'react';
+import { Container } from 'reactstrap';
+
 const JobSearch = (props) => {
-  return;
+  return <Container className='mt-2'></Container>;
 };
 
 export default JobSearch;
